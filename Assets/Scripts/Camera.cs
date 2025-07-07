5c026e6c5209cc4963b51ea4268ce7cd8d46655a
fer._irst <thegodanubis6@gmail.com>
2025-07-07 21:40:56 +0700

    7/07/2025
    - Fixed Resolution Canvas to Full HD
    + Add New Scene "Facial Detection"
    + Add New Script "Camera"

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField] private RawImage img = default;
+
+    private WebCamTexture webCam;
+
+    private void Start()
+    {
+        webCam = new WebCamTexture();
+        if (!webCam.isPlaying) webCam.Play();
+        img.texture = webCam;
+    }
+}
